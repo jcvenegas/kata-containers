b65a544a1cc06f8e0e6589a3a67b3a0b1fb27a8f
Carlos Venegas <jos.c.venegas.munoz@intel.com>
2021-06-01 20:00:17 +0000

    fix sudo permissions
    Signed-off-by: Carlos Venegas <jos.c.venegas.munoz@intel.com>

--- kata-deploy/kata-deploy.sh
+++ kata-deploy/kata-deploy.sh
@@ -2,7 +2,12 @@
 kata_dir=$(realpath "${PWD}/..")
 uid=$(id -u ${USER})
 gid=$(id -g ${USER})
-docker build -t build-kata-deploy --build-arg IMG_USER="${USER}"  .
+
+docker build -t build-kata-deploy \
+	--build-arg IMG_USER="${USER}"\
+	--build-arg UID=${uid}\
+	--build-arg GID=${gid}\
+	.
 docker run -i \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	--user ${uid}:${gid} \
