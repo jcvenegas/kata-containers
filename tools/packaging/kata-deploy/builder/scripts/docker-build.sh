589a6c17f86976c4a6fe90d63bbd52489f8fdc2f
Carlos Venegas <jos.c.venegas.munoz@intel.com>
2021-06-03 05:37:43 +0000

    kata-deploy: add build cli
    Signed-off-by: Carlos Venegas <jos.c.venegas.munoz@intel.com>

--- tools/packaging/kata-deploy/builder/scripts/docker-build.sh
+++ tools/packaging/kata-deploy/builder/scripts/docker-build.sh
@@ -3,5 +3,7 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 tool_dir=$(realpath ${script_dir}/..)
 
-docker run --rm --user "$(id -u)":"$(id -g)" -v "$tool_dir":"${tool_dir}" -w "${tool_dir}" rust:alpine cargo build
+container_image=kata-deploy-builder-build-env
+docker build -t "${container_image}" "${script_dir}"
+docker run --rm --user "$(id -u)":"$(id -g)" -v "$tool_dir":"${tool_dir}" -w "${tool_dir}" "${container_image}" cargo build
 cp "${tool_dir}/target/debug/kata-deploy-builder" .
---
+++ tools/packaging/kata-deploy/builder/src/cmd/src/lib.rs
@@ -0,0 +1,62 @@
+use clap::{AppSettings, Clap};
+use thiserror::Error;
+
+pub type Result<T> = std::result::Result<T, CliError>;
+
+#[derive(Clap)]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct Opts {
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    #[clap(subcommand)]
+    pub subcmd: SubCommand,
+}
+
+impl Opts {
+    pub fn get_log_level(self) -> LogLevel {
+        match self.verbose {
+            0 => LogLevel::None,
+            1 => LogLevel::Error,
+            2 => LogLevel::Warn,
+            3 => LogLevel::Info,
+            4 => LogLevel::Debug,
+            _ => LogLevel::Trace,
+        }
+    }
+}
+
+#[derive(Clap)]
+pub enum SubCommand {
+    Build(BuildCmd),
+}
+
+/// Build kata-deploy image
+#[derive(Clap)]
+pub struct BuildCmd {
+    pub asset: String,
+}
+
+//impl BuildCmd{
+//    fn exec(self) -> Result<()>{
+//    }
+//}
+//
+pub enum LogLevel {
+    None,
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+}
+
+#[derive(Debug, Error)]
+pub enum CliError {
+    #[error("failed to parse cli")]
+    FailedToParse,
+}
+
+pub fn parse_args() -> Result<Opts> {
+    let opts: Opts = Opts::parse();
+    Ok(opts)
+}
