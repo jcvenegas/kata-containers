36948bc9038a67bd86ab7adabfd051a34b177cd7
Carlos Venegas <jos.c.venegas.munoz@intel.com>
2021-06-23 22:03:11 +0000

    build: rename  merge script
    Signed-off-by: Carlos Venegas <jos.c.venegas.munoz@intel.com>

--- tools/packaging/kata-deploy/local-build/kata-deploy-merge-builds.sh
+++ tools/packaging/kata-deploy/local-build/kata-deploy-merge-builds.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# Copyright (c) 2019 Intel Corporation
+# Copyright (c) 2021 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -o errexit
+set -o nounset
 set -o pipefail
+set -o errtrace
 
-pushd kata-artifacts >>/dev/null
-for c in ./*.tar.xz
+kata_dir=${1:-build}
+
+cd "${kata_dir}"
+for c in ./kata-static-*.tar.xz
 do
     echo "untarring tarball $c"
     tar -xvf $c
 done
 
 tar cvfJ ../kata-static.tar.xz ./opt
-popd >>/dev/null
